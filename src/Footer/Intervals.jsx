9bda7f54c136b0610c8f53bf531d17473d813126
lukashyna <anastasiia.lukashyna@gmail.com>
2024-01-26 20:54:51 +0200

    recharts, info, intervals

--- src/Footer/Intervals.jsx
+++ src/Footer/Intervals.jsx
@@ -1,11 +1,29 @@
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Stack from "react-bootstrap/Stack";
+import { INTERVALS } from "./constants";
 
-function Intervals() {
+function Intervals({ activeHour, setActiveHour }) {
   return (
     <Row>
       <Col>
-        <p>I want to consume</p>
+        <Stack
+          direction="horizontal"
+          gap={2}
+          className="justify-content-center"
+        >
+          {INTERVALS.map(({ id, name }) => (
+            <Button
+              key={id}
+              variant="outline-warning"
+              active={(activeHour || 1) === id}
+              onClick={() => setActiveHour(id)}
+            >
+              {name}
+            </Button>
+          ))}
+        </Stack>
       </Col>
     </Row>
   );
