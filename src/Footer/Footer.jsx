10bacfb6ec01dbf809a0e8c842a80c6abc5c8c2b
lukashyna <anastasiia.lukashyna@gmail.com>
2024-01-26 14:28:50 +0200

    refactor constants

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,8 +1,13 @@
 import TargetHigh from "./TargetHigh";
 import TargetLow from "./TargetLow";
+import { DEFAULT_ACTIVE_BUTTON } from "../Head/constants";
 
 function Footer({ activePrice }) {
-  return <>{activePrice === "low" ? <TargetLow /> : <TargetHigh />}</>;
+  return (
+    <>
+      {activePrice === DEFAULT_ACTIVE_BUTTON ? <TargetLow /> : <TargetHigh />}
+    </>
+  );
 }
 
 export default Footer;
