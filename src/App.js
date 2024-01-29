5110d8eb78f48046b6c72dabf4421a35e405f3c0
lukashyna <anastasiia.lukashyna@gmail.com>
2024-01-29 19:42:05 +0200

    rightsidebar, form, submit event - clean up

--- src/App.js
+++ src/App.js
@@ -4,20 +4,29 @@ import Container from "react-bootstrap/Container";
 import Head, { DEFAULT_ACTIVE_BUTTON } from "./Head";
 import Body from "./Body";
 import Footer from "./Footer";
+import LeftSideBar from "./LeftSideBar";
 
 function App() {
   const [activePrice, setActivePrice] = useState(DEFAULT_ACTIVE_BUTTON);
   const [activeHour, setActiveHour] = useState(); ////undefined
+  const [showSideBar, setShowSideBar] = useState(false);
 
+  const handleOpenSideBar = () => setShowSideBar(true);
+  const handleCloseSideBar = () => setShowSideBar(false);
   return (
     <Container>
-      <Head activePrice={activePrice} setActivePrice={setActivePrice} />
+      <Head
+        activePrice={activePrice}
+        setActivePrice={setActivePrice}
+        handleOpenSideBar={handleOpenSideBar}
+      />
       <Body activeHour={activeHour} />
       <Footer
         activePrice={activePrice}
         activeHour={activeHour}
         setActiveHour={setActiveHour}
       />
+      <LeftSideBar show={showSideBar} handleClose={handleCloseSideBar} />
     </Container>
   );
 }
