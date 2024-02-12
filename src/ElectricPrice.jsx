198954d6e681d33dc5fc0290f95df1a30a6d44ef
lukashyna <anastasiia.lukashyna@gmail.com>
2024-02-12 21:01:04 +0200

    redux state

--- src/ElectricPrice.jsx
+++ src/ElectricPrice.jsx
@@ -1,22 +1,20 @@
 import { useEffect, useState } from "react";
 import "./App.scss";
 import Container from "react-bootstrap/Container";
-import Head, { DEFAULT_ACTIVE_BUTTON } from "./Head";
+import Head from "./Head";
 import Body from "./Body";
 import Footer from "./Footer";
 import LeftSideBar from "./LeftSideBar";
-import { getDefaultFrom, getDefaultUntil } from "./utils/dates";
 import ErrorModal from "./ErrorModal";
 import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { setActiveHour } from "./services/stateService";
 
 function ElectricPrice() {
   const params = useParams();
+  const dispatch = useDispatch();
 
-  const [activePrice, setActivePrice] = useState(DEFAULT_ACTIVE_BUTTON);
-  const [activeHour, setActiveHour] = useState(1);
   const [showSideBar, setShowSideBar] = useState(false);
-  const [from, setFrom] = useState(getDefaultFrom());
-  const [until, setUntil] = useState(getDefaultUntil());
   const [errorMessage, setErrorMessage] = useState(null);
   const [bestUntil, setBestUntil] = useState(0);
 
@@ -24,37 +22,17 @@ function ElectricPrice() {
   const handleCloseSideBar = () => setShowSideBar(false);
 
   useEffect(() => {
-    if (params.hours) setActiveHour(+params.hours);
-  }, [params]);
+    if (params.hours) dispatch(setActiveHour(+params.hours));
+  }, [params, dispatch]);
   return (
     <Container>
       <Head
-        activePrice={activePrice}
-        setActivePrice={setActivePrice}
         handleOpenSideBar={handleOpenSideBar}
         setErrorMessage={setErrorMessage}
       />
-      <Body
-        activeHour={activeHour}
-        from={from}
-        until={until}
-        setErrorMessage={setErrorMessage}
-        setBestUntil={setBestUntil}
-      />
-      <Footer
-        activePrice={activePrice}
-        activeHour={activeHour}
-        setActiveHour={setActiveHour}
-        bestUntil={bestUntil}
-      />
-      <LeftSideBar
-        show={showSideBar}
-        handleClose={handleCloseSideBar}
-        from={from}
-        until={until}
-        setFrom={setFrom}
-        setUntil={setUntil}
-      />
+      <Body setErrorMessage={setErrorMessage} setBestUntil={setBestUntil} />
+      <Footer bestUntil={bestUntil} />
+      <LeftSideBar show={showSideBar} handleClose={handleCloseSideBar} />
       <ErrorModal
         show={!!errorMessage}
         handleClose={() => setErrorMessage(null)}
